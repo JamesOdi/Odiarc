a640af64c8c15fee5f656a3d8627664f1ca75094
James <jamesodike26@gmail.com>
2022-01-31 12:20:26 +0100

    Add chip bottom navigation bar

---
+++ app/src/main/java/odiarc/com/views/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package odiarc.com.views.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import odiarc.com.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
